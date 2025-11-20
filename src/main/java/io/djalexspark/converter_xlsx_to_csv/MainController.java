ba83aeec5cf3c97c7d272cd58df8c0a73aab4bd4
DjAlexSpark <alex12595@mail.ru>
2025-11-20 23:52:49 +1000

    Работает
    вроде работает, скомплил стандартной идееей, через добавление мавеновских, java и jfx либ, вышел тяжелый на 139 МБ

--- src/main/java/io/djalexspark/converter_xlsx_to_csv/MainController.java
+++ src/main/java/io/djalexspark/converter_xlsx_to_csv/MainController.java
@@ -1,8 +1,166 @@
 package io.djalexspark.converter_xlsx_to_csv;
 
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-public class MainController {
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ResourceBundle;
 
+import static io.djalexspark.converter_xlsx_to_csv.TestMain.convertXlsxToCsv;
+
+public class MainController implements Initializable {
+    Stage stage;
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label statusLabel;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Button buttonDirectoryIntoChooser;
+
+    @FXML
+    private Button buttonFileFromChooser;
+
+    @FXML
+    private Button buttonStart;
+
+    @FXML
+    private TextField textFieldDirectoryInto;
+
+    @FXML
+    private TextField textFieldOutputName;
+
+    @FXML
+    private TextField textFieldWithName;
+
+    @FXML
+    void buttonDirectoryIntoChooserOnAction(ActionEvent event) {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Выберите папку для сохранения");
+        File defaultDirectory = new File("C:\\");
+        chooser.setInitialDirectory(defaultDirectory);
+        File selectedDirectory = chooser.showDialog(stage);
+
+        if (selectedDirectory!=null){
+            if (selectedDirectory.exists()){
+                textFieldDirectoryInto.setText(selectedDirectory.getAbsolutePath());
+                textFieldDirectoryInto.setStyle("");
+            }}
+    }
+
+    @FXML
+    void buttonFileFromChooserOnAction(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Выбрать файл excel");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Xlsx, xlsx", "*.xlsx" ),
+                new FileChooser.ExtensionFilter("All Files", "*.*"));
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        if (selectedFile != null) {
+            textFieldWithName.setText(selectedFile.getAbsolutePath());
+            textFieldWithName.setStyle("");
+        }
+    }
+
+    @FXML
+    void textFieldTakeFileFromOnKeyTyped(KeyEvent event) {
+        try {
+//            System.out.println(((TextField)event.getSource()).getText());
+            ((TextField) event.getSource()).getStyle();
+            if(Files.notExists(Path.of(((TextField) event.getSource()).getText()))){
+                ((TextField) event.getSource()).setStyle("-fx-text-fill:#e85460");
+            }else{((TextField) event.getSource()).setStyle("");}
+            ((TextField)event.getSource()).getText();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        buttonStart.setOnAction(event->{
+            buttonStart.setDisable(true);
+            if (!textFieldOutputName.getText().trim().isEmpty()&&
+                    Files.isRegularFile(Path.of(textFieldWithName.getText().trim()))&&
+                    !textFieldDirectoryInto.getText().isEmpty()&&Files.exists(Path.of(textFieldDirectoryInto.getText().trim()))){
+                //процесс конвертации тут
+                Path pathFrom = Path.of(textFieldWithName.getText().trim());
+                Path pathTo = Path.of(textFieldDirectoryInto.getText().trim()+"\\"+textFieldOutputName.getText().trim()+".csv");
+                convertXlsxToCsv(pathFrom,pathTo);
+                    statusLabel.setText("Конвертирование завершено");
+
+
+            }else {if (textFieldWithName.getText().isEmpty()) {
+                textFieldWithName.setStyle("-fx-border-color:red");
+            }else {
+                textFieldWithName.setStyle("");
+            }
+                if (textFieldOutputName.getText().isEmpty()) {
+                    textFieldOutputName.setStyle("-fx-border-color:red");
+                }else {
+                    textFieldOutputName.setStyle("");
+                }
+                if (textFieldDirectoryInto.getText().isEmpty()) {
+                    textFieldDirectoryInto.setStyle("-fx-border-color:red");
+                }else {
+                    textFieldDirectoryInto.setStyle("");
+                }}
+
+            buttonStart.setDisable(false);
+
+            Task<Void> sleeper = new Task<Void>() {
+                @Override
+                protected Void call() throws Exception {
+                    try {
+                        Thread.sleep(3500);
+                    } catch (InterruptedException e) {
+                    }
+                    return null;
+                }
+            };
+            sleeper.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+                @Override
+                public void handle(WorkerStateEvent event) {
+                    statusLabel.setText("");
+                }
+            });
+            Thread thread = new Thread(sleeper);
+            thread.setDaemon(true);
+            thread.start();
+        });
+
+
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module io.djalexspark.converter_xlsx_to_csv {
     requires javafx.controls;
     requires javafx.fxml;
-//    requires poi.ooxml.schemas;
-    requires poi;
+
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
+
+    requires java.desktop;
+
 
     opens io.djalexspark.converter_xlsx_to_csv to javafx.fxml;
     exports io.djalexspark.converter_xlsx_to_csv;
